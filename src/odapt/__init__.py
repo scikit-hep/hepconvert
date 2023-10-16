bcc1309413b51506fa5d02bf33cd8b9aed3705f9
zbilodea <70441641+zbilodea@users.noreply.github.com>
2023-10-16 05:53:09 -0400

    Change name (#3)
    * basic iterative and not-quite tree reduction hadd functions started
    
    * Variables now stored in np array, some flexibility added (directories, finding histogram names etc.) removed attempt at tree reduction
    
    * Changed to adding numpy arrays, added to docstrings, changed structure for multiple histograms per file (still untested though)
    
    * Setting up tests and module
    
    * small fixes for different cases, added tests
    
    * Added a sort of partial tree reduction and tests
    
    * rewrote to work smoother, added options from ROOT hadd and started commandline arguments
    
    * name change
    
    * removing extra changes
    
    * format change
    
    * fix version file
    
    * Delete src/odapt/_version.pyi
    
    * format fix
    
    * fix attempt
    
    * fix attempt
    
    * version fix
    
    * finally fixed
    
    * version not recognized
    
    * version still not recognized
    
    * version
    
    * fixing version
    
    * more name changes

---
+++ src/odapt/__init__.py
@@ -0,0 +1,10 @@
+"""
+Copyright (c) 2023 Zoë Bilodeau. All rights reserved.
+
+Odapt: File conversion package.
+"""
+from __future__ import annotations
+
+from odapt._version import version as __version__
+
+__all__ = ["__version__"]
