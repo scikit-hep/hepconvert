3b37e6722628e1d71cfa5421fdd233f2303415c8
Zoë Bilodeau <zobilodeau@gmail.com>
2023-10-16 10:34:13 +0200

    version fix

--- src/odapt/__init__.py
+++ src/odapt/__init__.py
@@ -4,6 +4,6 @@ Copyright (c) 2023 Zoë Bilodeau. All rights reserved.
 Odapt: File conversion package.
 """
 
-from version import __version__
+from Odapt.version import __version__
 
 __all__ = ["__version__"]
