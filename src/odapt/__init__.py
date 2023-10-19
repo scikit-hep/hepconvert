2b331c7467ae138a5f2d63547dd04d2eff8218a0
Zoë Bilodeau <zobilodeau@gmail.com>
2023-10-19 14:34:13 +0200

    trying to fix pylint naming convention error

--- src/odapt/__init__.py
+++ src/odapt/__init__.py
@@ -6,6 +6,6 @@ odapt: File conversion package.
 from __future__ import annotations
 
 from odapt.operations import hadd  # noqa: F401
-from odapt.version import version as __version__
+from odapt.version import VERSION as __version__
 
 __all__ = ["__version__"]
--- src/odapt/version.py
+++ src/odapt/version.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import re
 
 __version__ = "1.0.1"
-version = __version__ # pylint: disable=invalid-name
+VERSION = __version__ # pylint: disable=invalid-name
 version_info = tuple(re.split(r"[-\.]", __version__))
 
 del re
