f7979450e1b009747a63ff2930efa97490b089a2
zbilodea <70441641+zbilodea@users.noreply.github.com>
2023-10-19 08:21:02 -0400

    Update version.py

--- src/odapt/version.py
+++ src/odapt/version.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import re
 
 __version__ = "1.0.1"
-version = __version__
+version = __version__ #noqa: C0103
 version_info = tuple(re.split(r"[-\.]", __version__))
 
 del re
