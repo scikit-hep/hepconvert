600391f336c5c74c2d6d8f8a562a6e155837770d
Zoë Bilodeau <zobilodeau@gmail.com>
2023-10-20 09:49:47 +0200

    changing version again

--- src/odapt/_version.py
+++ src/odapt/_version.py
@@ -2,18 +2,18 @@
 # don't change, don't track in version control
 from __future__ import annotations
 
-TYPE_CHECKING = False
-if TYPE_CHECKING:
-    from typing import Tuple, Union
+# TYPE_CHECKING = False
+# if TYPE_CHECKING:
+#     from typing import Tuple, Union
 
-    VERSION_TUPLE = Tuple[Union[int, str], ...]
-else:
-    VERSION_TUPLE = object
+#     VERSION_TUPLE = Tuple[Union[int, str], ...]
+# else:
+#     VERSION_TUPLE = object
 
-version: str
-__version__: str
-__version_tuple__: VERSION_TUPLE
-version_tuple: VERSION_TUPLE
+# version: str
+# __version__: str
+# __version_tuple__: VERSION_TUPLE
+# version_tuple: VERSION_TUPLE
 
 __version__ = version = "1.0.2"
 __version_tuple__ = version_tuple = (1, 0, 2)
