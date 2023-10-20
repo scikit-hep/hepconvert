50c396cf38fd7caee1ea329de2ffb627be5f4664
Zoë Bilodeau <zobilodeau@gmail.com>
2023-10-20 09:21:57 +0200

    fixing version file name

--- src/odapt/__init__.py
+++ src/odapt/__init__.py
@@ -5,7 +5,7 @@ odapt: File conversion package.
 """
 from __future__ import annotations
 
+from odapt._version import VERSION as __version__
 from odapt.operations import hadd  # noqa: F401
-from odapt.version import VERSION as __version__
 
 __all__ = ["__version__"]
