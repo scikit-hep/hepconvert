8fa07fe27f5f553a986f3bbf9edc67b25dfec8e2
Zoë Bilodeau <zobilodeau@gmail.com>
2023-11-07 11:06:35 +0100

    Can merge certain files, but has the copy issue to be resolved

--- src/odapt/operations/merge.py
+++ src/odapt/operations/merge.py
@@ -2,7 +2,7 @@ import uproot
 import hadd
 import numpy as np
 import awkward as ak
-def merge_files(destination, file1, file2, step_size="100MB"): #hadd includes
+def merge_files(destination, file1, file2, step_size="100MB", *, force=True, append=False, compression='LZ4', compression_level=1, skip_bad_files=False, union=True, batch=False): #hadd includes
     """
     Args:
         destination (path-like): Name of the output file or file path.
@@ -22,8 +22,8 @@ def merge_files(destination, file1, file2, step_size="100MB"): #hadd includes
             this gets set to system limit.
         union (bool): If True, adds the histograms that have the same name and copies all others
             to the new file.
-        same_names (bool): If True, only adds together histograms which have the same name (key). If False,
-            histograms are added together based on TTree structure (bins must be equal).
+        batch (bool): If True, branches and TTrees (when applicable) are written to the out-file in 
+        batches of size defined by the step_size argument.
 
     Adds together histograms from local ROOT files of a collection of ROOT files, and writes them to
         a new or existing ROOT file.
@@ -33,7 +33,49 @@ def merge_files(destination, file1, file2, step_size="100MB"): #hadd includes
     """
     # Use tmpdir? Or just do two at a time, tree reduction style...
     import tempfile
+    if compression in ("ZLIB", "zlib"):
+        compression_code = uproot.const.kZLIB
+    elif compression in ("LZMA", "lzma"):
+        compression_code = uproot.const.kLZMA
+    elif compression in ("LZ4", "lz4"):
+        compression_code = uproot.const.kLZ4
+    elif compression in ("ZSTD", "zstd"):
+        compression_code = uproot.const.kZSTD
+    else:
+        msg = f"unrecognized compression algorithm: {compression}. Only ZLIB, LZMA, LZ4, and ZSTD are accepted."
+        raise ValueError(msg)
+    p = Path(destination)
+    if Path.is_file(p):
+        if not force and not append:
+            raise FileExistsError
+        if force and append:
+            msg = "Cannot append to a new file. Either force or append can be true."
+            raise ValueError(msg)
+        file_out = uproot.recreate(
+            destination,
+            compression=uproot.compression.Compression.from_code_pair(
+                compression_code, compression_level
+            ),
+        )
+    else:
+        if append:
+            raise FileNotFoundError(
+                "File %s" + destination + " not found. File must exist to append."
+            )
+        file_out = uproot.recreate(
+            destination,
+            compression=uproot.compression.Compression.from_code_pair(
+                compression_code, compression_level
+            ),
+        )
+    
+    if not isinstance(files, list):
+        path = Path(files)
+        files = sorted(path.glob("**/*.root"))
 
+    if len(files) <= 1:
+        msg = "Cannot hadd one file. Use root_to_root to copy a ROOT file."
+        raise ValueError(msg) from None
     f1 = uproot.open(file1)
     f2 = uproot.open(file2)
     #title must be the same as the file name? maybe is just a tChain thing
@@ -48,7 +90,6 @@ def merge_files(destination, file1, file2, step_size="100MB"): #hadd includes
     additional_keys = np.setdiff1d(t2_keys, shared_keys)
 
     for key in shared_keys:
-        # print("Shared keys!", out_file.keys())
         if isinstance(f1[key], uproot.TTree) and isinstance(f2[key], uproot.TTree):
             try:
                 f1[key].name
@@ -57,7 +98,8 @@ def merge_files(destination, file1, file2, step_size="100MB"): #hadd includes
                 raise uproot.KeyInFileError
             if f1[key].name != f2[key].name:
                 print("Names must be the same")
-            recur_merge(destination, out_file, f1[key], f2[key], step_size)
+            
+            recur_merge(destination, out_file, f1, f2, key, step_size)
             # finally: 
             # Write just f1 to file???
             # msg = "Files must have similar structure."
@@ -81,7 +123,8 @@ def merge_files(destination, file1, file2, step_size="100MB"): #hadd includes
             # hadd.hadd_merge(destination, f1, key, first=True, n_key=None) #tree1? does it need to be a file?
         else:
             first = True
-            for chunk in uproot.iterate(f1[key]):
+            for chunk in uproot.iterate(f1[key], step_size=step_size, how=dict):
+                print("iterating?")
                 if first:
                     first = False
                     out_file[key] = chunk
@@ -89,13 +132,10 @@ def merge_files(destination, file1, file2, step_size="100MB"): #hadd includes
                     out_file[key].extend(chunk)
 
     for key in missing_keys:
-        print("Missing keys", missing_keys)
-        # print("outfile: ", out_file.keys(recursive=False))
         if isinstance(f1[key], uproot.TTree):
             tree = f1[key]
             branches = tree.branches 
             dtypes = []
-            # print(branches)
             for i in branches:
                 if isinstance(i.interpretation, uproot.AsJagged):
                     dtypes.append(i.interpretation.content.numpy_dtype)
@@ -106,16 +146,6 @@ def merge_files(destination, file1, file2, step_size="100MB"): #hadd includes
             out_file.mktree(tree.name, branch_types=nametypes)
             
             recur_copy(destination, out_file, f1[key], step_size)
-            # # print("line 102:", out_file.keys(recursive=False))
-            # first = True
-            # for branch in branches:
-            #     first = True
-            #     for chunk in branch.iterate(step_size=step_size, how=dict):
-            #         if first:
-            #             first = False
-            #             out_file[branch.name] = chunk
-            #         else: 
-            #             out_file[branch.name].extend(chunk)
 
         elif f1[key].classname.startswith("TH") or f1[key].classname.startswith("TProfile"):
             print("Histogram")
@@ -129,24 +159,30 @@ def merge_files(destination, file1, file2, step_size="100MB"): #hadd includes
                 out_file[key] = hadd.hadd_3d(destination, f1, key, True)
         
         else:
-            first = True
-            for i in uproot.TBranch.iterate(tree[key], step_size=step_size, how=dict):
-                if first:
-                    first = False
-                    out_file[key] = i
-                out_file[key].extend(i)
+            try:
+                f1[key].iteritems()
+                first = True
+                for i in uproot.TBranch.iterate(f1[key], step_size=step_size, how=dict):
+                    if first:
+                        first = False
+                        out_file[key] = i
+                    out_file[key].extend(i)
+            except AttributeError:
+                Warning("ROOT class ", f1[key].classname, " cannot be iterated over.")
+                try:
+                    out_file[key] = f1[key]
+                except NotImplementedError:
+                    Warning("Cannot write type ", f1[key].classname, " skipping branch ", key)
+            
     for key in additional_keys:
-        # print("Additional keys", additional_keys)
-        # print("outfile in additional keys:", out_file.keys(recursive=False))
         if isinstance(f2[key], uproot.TTree):
             tree = f2[key]
             branches = tree.branches 
             dtypes = []
-            # print(branches)
             for i in branches:
                 if isinstance(i.interpretation, uproot.AsJagged):
                     dtypes.append(i.interpretation.content.numpy_dtype)
-                else:
+                else: 
                     dtypes.append(i.interpretation.numpy_dtype)
 
             nametypes = {branches[i].name : dtypes[i] for i, value in enumerate(branches)}
@@ -164,7 +200,7 @@ def merge_files(destination, file1, file2, step_size="100MB"): #hadd includes
             else:
                 h_sum = hadd.hadd_3d(destination, f2, key, True)
 
-            out_file[key] = h_sum
+            # out_file[key] = h_sum
         else:
             first = True
             for i in uproot.TBranch.iterate(tree[key], step_size=step_size, how=dict):
@@ -172,6 +208,7 @@ def merge_files(destination, file1, file2, step_size="100MB"): #hadd includes
                     first = False
                     out_file[key] = i
                 out_file[key].extend(i)
+
         # write...
     #   read key - get get class name
     #   inputs(?) = tlist()
@@ -184,10 +221,8 @@ def merge_files(destination, file1, file2, step_size="100MB"): #hadd includes
 
 def recur_copy(destination, out_file, tree, step_size):
     keys = tree.keys(recursive=False) #since cycle was false for the tree the first time, may break/be unnecessary here
-    print(keys)
     for key in keys:
         if isinstance(tree[key], uproot.TTree):
-            print("ttree")
             sub_tree = tree[key]
             branches = sub_tree.branches 
             dtypes = []
@@ -202,7 +237,7 @@ def recur_copy(destination, out_file, tree, step_size):
 
             for branch in branches: # use keys instead for calling recursive function
                 if isinstance(branch.classname, uproot.TTree):
-                    recur_copy(destination, out_file, branch, step_size)
+                    recur_copy(destination, out_file, branch, step_size)   
                 else:
                     first = True
                     for chunk in branch.iterate(step_size=step_size, how=dict):
@@ -232,60 +267,74 @@ def recur_copy(destination, out_file, tree, step_size):
                     out_file[tree.name] = i
                 out_file[tree.name].extend(i)
 
-def recur_merge(out_file, tree1, tree2, key, step_size): 
-    print("Recur")
+def recur_merge(destination, out_file, f1, f2, k, step_size): 
+    print("Recur merge")
+    tree1 = f1[k]
+    tree2 = f2[k]
     t1_keys = tree1.keys(recursive=False)
-
     t2_keys = tree2.keys(recursive=False)
     
     shared_keys = np.intersect1d(t1_keys, t2_keys)
     missing_keys = np.setdiff1d(t1_keys, shared_keys)
     additional_keys = np.setdiff1d(t2_keys, shared_keys)
-    # out_file.mktree(tree1.name, tree1.typenames(recursive=False)) #will tree typenames be the same? Don't think so...
+    # out_file.mktree(tree1.name, tree1.typenames(recursive=False))
     # get keys for branches? for hadd
-    for key in shared_keys:
-        classname = tree1.classname
-        if isinstance(classname, uproot.TTree) and isinstance(tree2.class_name(), uproot.TTree):
-            if tree1.name == tree2.name:
-                # Iterate here?
-                # Need Union of Tbaskets or something...?
-                out_file[key][tree1.name] = tree1
-        # elif isinstance(f[key], uproot.histogram)
-        elif classname.startswith("TH") or classname.startswith("TProfile"):
-            if len(file[key].axes) == 1:
-                out_file[key] = hadd.hadd_1d(file1, tree2[key], key, False)
+    branches = tree1.branches
+    num_branches = len(tree1.items())
+    dtypes1 = [None for i in range(num_branches)]
+    for i in range(num_branches):
+        if isinstance(branches[i].interpretation, uproot.AsJagged):
+            dtypes1[i] = branches[i].interpretation.content.numpy_dtype
+        else:
+            dtypes1[i] = branches[i].interpretation.numpy_dtype
 
-            elif len(file[key].axes) == 2:
-                out_file[key] = hadd.hadd_2d(file1, tree2[key], key, False)
+    branches = tree2.branches
+    num_branches = len(tree2.items())
+    dtypes2 = [None for i in range(num_branches)]
+    for i in range(num_branches):
+        if isinstance(branches[i].interpretation, uproot.AsJagged):
+            dtypes2[i] = branches[i].interpretation.content.numpy_dtype
+        else:
+            dtypes2[i] = branches[i].interpretation.numpy_dtype
+
+    dtypes = np.union1d(dtypes1, dtypes2)
+    nametypes = {branches[i].name : dtypes[i] for i, value in enumerate(dtypes)}
+    out_file.mktree(tree1.name, branch_types=nametypes)
+    first = True
+    for chunk in uproot.iterate(tree1, step_size=step_size, how=dict):
+        if first:
+            out_file[k] = chunk
+        out_file[k].extend(chunk)
+    # for chunk in uproot.iterate(tree2, step_size=step_size, how=dict):
+    #     out_file[k].extend(chunk)
+    for key in shared_keys:
+        if isinstance(tree1[key], uproot.TTree) and isinstance(tree2[key], uproot.TTree):
+            if tree1[key].name != tree2[key].name:
+                print("Names must be the same")
+            
+            recur_merge(destination, out_file, tree1[key], tree2[key], key, step_size)
+
+            # finally: 
+            # Write just f1 to file???
+            # msg = "Files must have similar structure."
+            # raise ValueError(msg) from None
+        
+        elif tree1.classname.startswith("TH") or tree1.classname.startswith("TProfile"): # And check tree2 I guess...
+            import tempfile
+
+            if len(file[k].axes) == 1:
+                #Make temp, add them together...then write to out_file
+                fp = tempfile.TemporaryFile()
+                out_file[k] = hadd.hadd_1d(f1, tree2, k, True)
+
+            elif len(file[k].axes) == 2:
+                out_file[k] = hadd.hadd_2d(f1, tree2, k, True)
 
             else:
-                out_file[key] = hadd.hadd_3d(file1, tree2[key], key, False)
+                out_file[k] = hadd.hadd_3d(f1, tree2, k, True)
             
-        else:
-            # print(tree1[key].typenames(recursive=False))
-            first = True
-            for i in uproot.TBranch.iterate(tree1[key], step_size=step_size, how=dict):
-                if first:
-                    first = False
-                    out_file[key] = i
-                out_file[key].extend(i)
 
-    for key in missing_keys:
-        if isinstance(tree1[key], uproot.TTree):
-            tree = tree1[key]
-            in_keys = tree.keys(recursive=False)
-            out_file.mktree(tree.name, tree.typenames(recursive=False))
-            out_file[key].extend({in_key: i for i in uproot.iterate(tree[missing_keys], step_size=step_size)})
-            for chunk in tree[in_key].iterate(step_size=step_size):
-                print("chunk")
-
-    for key in additional_keys:
-        if isinstance(tree2[key], uproot.TTree):
-            tree = tree2[key]
-            in_keys = tree.keys(recursive=False)
-            out_file.mktree(tree.name, tree.typenames(recursive=False))
-            for in_key in tree.keys(recursive=False):
-                out_file[key].extend({in_key: i for i in uproot.iterate(tree[in_key], step_size=step_size)})
+            # hadd.hadd_merge(destination, f1, key, first=True, n_key=None) #tree1? does it need to be a file?
 
 
 def convert_typenames(dict):
@@ -312,19 +361,19 @@ temp = uproot.recreate("testfile1.root")
 #         file1[keys[0]].extend(chunk)
 # print(file1.keys(recursive=True, cycle=False))
 
-merge_files(
-    "uproot-HZZ-hepdata.root",
-    data_path("uproot-hepdata-example.root"),
-    data_path("uproot-HZZ.root")
-)
+f = uproot.open(data_path("uproot-Event.root"))
+print(f.keys())
+print(f["ProcessID0"])
 
-out_file = uproot.open("uproot-HZZ-hepdata.root")
-# print(out_file.keys(recursive=True))
-keys = out_file.keys(cycle=False, recursive=False)
-print(out_file.keys(recursive=False, cycle=False))
+# merge_files(
+#     "test_dest.root",
+#     data_path("uproot-HZZ.root"),
+#     data_path("uproot-HZZ.root")
+# )
 
+out_file = uproot.open(data_path("uproot-HZZ.root"))
+keys = out_file.keys(cycle=False, recursive=True)
+print("hadd dest", out_file['events'].show())
 
-# test_file = uproot.open("/Users/zobil/Documents/odapt/src/odapt/operations/test.root")
-# print(test_file.keys(recursive=True))
-# keys = test_file.keys(cycle=False)
-# print(keys)
+test = uproot.open("test_dest.root")
+keys = test.keys(cycle=False, recursive=True)
