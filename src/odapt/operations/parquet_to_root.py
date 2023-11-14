58b1e1be64818160628b5eb8dc1c817b0e579996
Zoë Bilodeau <zobilodeau@gmail.com>
2023-11-14 15:54:38 +0100

    Progress, getting more into metadata

--- src/odapt/operations/parquet_to_root.py
+++ src/odapt/operations/parquet_to_root.py
@@ -16,15 +16,24 @@ def parquet_to_root(in_file,
     ):
     # dak.from_parquet(read_path, split_row_groups=True)
     metadata = ak.metadata_from_parquet(in_file)
+    # print("Columns?", type(metadata['form'].content))
+    # print("Columns?", metadata['form']._column_types())
     of = uproot.recreate(out_file)
     first = True
+    # of.mktree(branch_types={metadata['form'].fields: })
     for i in range(metadata['num_row_groups']): # So, we want Tbaskets to be about 1 mb? I forget. But something specific
         if first:
             print(type(ak.from_parquet(in_file, row_groups=[i])))
-            of['tree'] = ak.from_parquet(in_file, row_groups=[i])
+            data = ak.from_parquet(in_file, row_groups=[i])
+            print(i.fields)
+            of.mktree()
+            print(type(data))
+            # of['tree'] = 
             first = False
         else:
             of['tree'].extend(ak.from_parquet(in_file, row_groups=[i])) # Set size with extend.
     
 from skhep_testdata import data_path
+
+# parquet_to_root("/Users/zobil/Documents/odapt/tests/samples/parquet_test.parquet", "/Users/zobil/Documents/odapt/tests/samples/parquet_test.parquet")
 parquet_to_root("https://pivarski-princeton.s3.amazonaws.com/chicago-taxi.parquet", "large_out.root")
