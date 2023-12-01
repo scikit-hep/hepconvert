7bf45b6d13c69262688a5c54dd75aee471a6825b
Zoë Bilodeau <70441641+zbilodea@users.noreply.github.com>
2023-12-01 10:16:21 +0100

    Feat add ttree merger (#12)
    * beginning tree merger
    
    * progress
    
    * progress on merging
    
    * Very messy right now, but figured out the dtype issues that were preventing writing var * float...
    
    * Separated hadd and merge/add, merges better now but issue with uproot Model_TAxis_v9 writing back
    
    * save progress
    
    * Can merge certain files, but has the copy issue to be resolved
    
    * Simplified merge, need to work on grouping
    
    * saving progress: fixed duplicating counters, now working on extend issue with zipped data
    
    * Able to be built, tests pass
    
    * formatting
    
    * formatting
    
    * formatting
    
    * formatting
    
    * formatting
    
    * formatting, pylint having issues with similar code
    
    * formatting, pylint having issues with similar code
    
    * removed main hadd function
    
    * more formatting
    
    * import error fix
    
    * import error fix
    
    * import error fix
    
    * docs
    
    * docs

--- src/odapt/operations/__init__.py
+++ src/odapt/operations/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import annotations
 
-from odapt.operations.hadd import hadd, main  # noqa: F401
+from odapt.operations.hadd import hadd_1d, hadd_2d, hadd_3d  # noqa: F401
+from odapt.operations.merge import hadd_and_merge  # noqa: F401
--- src/odapt/operations/hadd.py
+++ src/odapt/operations/hadd.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
 
-import argparse
-from pathlib import Path
-
 import numpy as np
 import uproot
 
@@ -38,7 +35,14 @@ def hadd_1d(destination, file, key, first, *, n_key=None):
             hist.values(flow=True),
             *member_data,
             hist.variances(flow=True),
-            hist.member("fXaxis"),
+            uproot.writing.identify.to_TAxis(
+                "fXaxis",
+                "",
+                hist.member("fN"),
+                hist.axis(axis="x").low,
+                hist.axis(axis="x").high,
+                fXbins=hist.member("fXaxis").edges(flow=True),
+            ),
         )
     if hist.member("fN") == outfile[key].member("fN"):
         member_data = np.array(
@@ -67,7 +71,14 @@ def hadd_1d(destination, file, key, first, *, n_key=None):
                 member_data,
             ),
             outfile[key].variances(flow=True) + hist.variances(flow=True),
-            hist.member("fXaxis"),
+            uproot.writing.identify.to_TAxis(
+                "fXaxis",
+                "",
+                hist.member("fN"),
+                hist.axis(axis="x").low,
+                hist.axis(axis="x").high,
+                fXbins=hist.member("fXaxis").edges(flow=True),
+            ),
         )
         outfile.close()
         return h_sum
@@ -115,8 +126,21 @@ def hadd_2d(destination, file, key, first, *, n_key=None):
             np.ravel(hist.values(flow=True), order="C"),
             *member_data,
             np.ravel(hist.variances(flow=True), order="C"),
-            hist.member("fXaxis"),
-            hist.member("fYaxis"),
+            uproot.writing.identify.to_TAxis(
+                "fXaxis",
+                "",
+                hist.member("fXaxis").member("fNbins"),
+                hist.axis(axis="x").low,
+                hist.axis(axis="x").high,
+                fXbins=hist.member("fXaxis").edges(flow=True),
+            ),
+            uproot.writing.identify.to_TAxis(
+                "fYaxis",
+                "",
+                hist.member("fYaxis").member("fNbins"),
+                hist.axis(axis="y").low,
+                hist.axis(axis="y").high,
+            ),
         )
     if hist.member("fN") == outfile[key].member("fN"):
         member_data = np.array(
@@ -131,13 +155,11 @@ def hadd_2d(destination, file, key, first, *, n_key=None):
                 hist.member("fTsumwxy"),
             ]
         )
-
         h_sum = uproot.writing.identify.to_TH2x(
             hist.member("fName"),
             hist.member("fTitle"),
-            np.ravel(
-                outfile[key].values(flow=True) + hist.values(flow=True), order="C"
-            ),
+            np.ravel(outfile[key].values(flow=True), order="C")
+            + np.ravel(hist.values(flow=True), order="C"),
             *np.add(
                 np.array(
                     [
@@ -156,8 +178,21 @@ def hadd_2d(destination, file, key, first, *, n_key=None):
             np.ravel(
                 outfile[key].variances(flow=True) + hist.variances(flow=True), order="C"
             ),
-            hist.member("fXaxis"),
-            hist.member("fYaxis"),
+            uproot.writing.identify.to_TAxis(
+                "fXaxis",
+                "",
+                hist.member("fXaxis").member("fNbins"),
+                hist.axis(axis="x").low,
+                hist.axis(axis="x").high,
+                fXbins=hist.member("fXaxis").edges(flow=True),
+            ),
+            uproot.writing.identify.to_TAxis(
+                "fYaxis",
+                "",
+                hist.member("fYaxis").member("fNbins"),
+                hist.axis(axis="y").low,
+                hist.axis(axis="y").high,
+            ),
         )
         outfile.close()
         return h_sum
@@ -209,9 +244,28 @@ def hadd_3d(destination, file, key, first, *, n_key=None):
             np.ravel(hist.values(flow=True), order="C"),
             *member_data,
             np.ravel(hist.variances(flow=True), order="C"),
-            hist.member("fXaxis"),
-            hist.member("fYaxis"),
-            hist.member("fZaxis"),
+            uproot.writing.identify.to_TAxis(
+                "fXaxis",
+                "",
+                hist.member("fXaxis").member("fNbins"),
+                hist.axis(axis="x").low,
+                hist.axis(axis="x").high,
+                fXbins=hist.member("fXaxis").edges(flow=True),
+            ),
+            uproot.writing.identify.to_TAxis(
+                "fYaxis",
+                "",
+                hist.member("fYaxis").member("fNbins"),
+                hist.axis(axis="y").low,
+                hist.axis(axis="y").high,
+            ),
+            uproot.writing.identify.to_TAxis(
+                "fZaxis",
+                "",
+                hist.member("fZaxis").member("fNbins"),
+                hist.axis(axis="z").low,
+                hist.axis(axis="z").high,
+            ),
         )
     if hist.member("fN") == outfile[key].member("fN"):
         member_data = np.add(
@@ -255,13 +309,35 @@ def hadd_3d(destination, file, key, first, *, n_key=None):
                 outfile[key].values(flow=True) + hist.values(flow=True), order="C"
             ),
             *member_data,
-            np.ravel(
-                (outfile[key].variances(flow=True) + hist.variances(flow=True)),
-                order="C",
+            (
+                np.ravel(outfile[key].variances(flow=True), order="C")
+                + np.ravel(
+                    hist.variances(flow=True),
+                    order="C",
+                )
             ),
-            hist.member("fXaxis"),
-            hist.member("fYaxis"),
-            hist.member("fZaxis"),
+            uproot.writing.identify.to_TAxis(
+                "fXaxis",
+                "",
+                hist.member("fXaxis").member("fNbins"),
+                hist.axis(axis="x").low,
+                hist.axis(axis="x").high,
+                fXbins=hist.member("fXaxis").edges(flow=True),
+            ),
+            uproot.writing.identify.to_TAxis(
+                "fYaxis",
+                "",
+                hist.member("fYaxis").member("fNbins"),
+                hist.axis(axis="y").low,
+                hist.axis(axis="y").high,
+            ),
+            uproot.writing.identify.to_TAxis(
+                "fZaxis",
+                "",
+                hist.member("fZaxis").member("fNbins"),
+                hist.axis(axis="z").low,
+                hist.axis(axis="z").high,
+            ),
         )
         outfile.close()
         return h_sum
@@ -273,227 +349,3 @@ def hadd_3d(destination, file, key, first, *, n_key=None):
         " and ",
         outfile[key].member("fN"),
     ) from None
-
-
-def hadd(
-    destination,
-    files,
-    *,
-    force=True,
-    append=False,
-    compression="lz4",
-    compression_level=1,
-    skip_bad_files=False,
-    union=True,
-    same_names=False,
-):
-    """
-    Args:
-        destination (path-like): Name of the output file or file path.
-        files (Str or list of str): List of local ROOT files to read histograms from.
-            May contain glob patterns.
-        force (bool): If True, overwrites destination file if it exists. Force and append
-            cannot both be True.
-        append (bool): If True, appends histograms to an existing file. Force and append
-            cannot both be True.
-        compression (str): Sets compression level for root file to write to. Can be one of
-            "ZLIB", "LZMA", "LZ4", or "ZSTD". By default the compression algorithm is "LZ4".
-        compression_level (int): Use a compression level particular to the chosen compressor.
-            By default the compression level is 1.
-        skip_bad_files (bool): If True, skips corrupt or non-existent files without exiting.
-        max_opened_files (int): Limits the number of files to be open at the same time. If 0,
-            this gets set to system limit.
-        union (bool): If True, adds the histograms that have the same name and copies all others
-            to the new file.
-        same_names (bool): If True, only adds together histograms which have the same name (key). If False,
-            histograms are added together based on TTree structure (bins must be equal).
-
-    Adds together histograms from local ROOT files of a collection of ROOT files, and writes them to
-        a new or existing ROOT file.
-
-        >>> odapt.add_histograms("destination.root", ["file1_to_hadd.root", "file2_to_hadd.root"])
-
-    """
-    if compression in ("ZLIB", "zlib"):
-        compression_code = uproot.const.kZLIB
-    elif compression in ("LZMA", "lzma"):
-        compression_code = uproot.const.kLZMA
-    elif compression in ("LZ4", "lz4"):
-        compression_code = uproot.const.kLZ4
-    elif compression in ("ZSTD", "zstd"):
-        compression_code = uproot.const.kZSTD
-    else:
-        msg = f"unrecognized compression algorithm: {compression}. Only ZLIB, LZMA, LZ4, and ZSTD are accepted."
-        raise ValueError(msg)
-    p = Path(destination)
-    if Path.is_file(p):
-        if not force and not append:
-            raise FileExistsError
-        if force and append:
-            msg = "Cannot append to a new file. Either force or append can be true."
-            raise ValueError(msg)
-        file_out = uproot.recreate(
-            destination,
-            compression=uproot.compression.Compression.from_code_pair(
-                compression_code, compression_level
-            ),
-        )
-    else:
-        if append:
-            raise FileNotFoundError(
-                "File %s" + destination + " not found. File must exist to append."
-            )
-        file_out = uproot.recreate(
-            destination,
-            compression=uproot.compression.Compression.from_code_pair(
-                compression_code, compression_level
-            ),
-        )
-
-    if not isinstance(files, list):
-        path = Path(files)
-        files = sorted(path.glob("**/*.root"))
-
-    if len(files) <= 1:
-        msg = "Cannot hadd one file. Use root_to_root to copy a ROOT file."
-        raise ValueError(msg) from None
-
-    with uproot.open(files[0]) as file:
-        keys = file.keys(filter_classname="TH[1|2|3][I|S|F|D|C]", cycle=False)
-    if same_names:
-        if union:
-            for i, _value in enumerate(files[1:]):
-                with uproot.open(files[i]) as file:
-                    keys = np.union1d(
-                        keys,
-                        file.keys(filter_classname="TH[1|2|3][I|S|F|D|C]", cycle=False),
-                    )
-        else:
-            for i, _value in enumerate(files[1:]):
-                with uproot.open(files[i]) as file:
-                    keys = np.intersect1d(
-                        keys,
-                        file.keys(filter_classname="TH[1|2|3][I|S|F|D|C]", cycle=False),
-                    )
-    else:
-        keys = file.keys(filter_classname="TH[1|2|3][I|S|F|D|C]", cycle=False)
-
-    first = True
-    for input_file in files:
-        p = Path(input_file)
-        if Path.is_file(p):
-            file_out = uproot.update(destination)
-        else:
-            file_out = uproot.recreate(
-                destination,
-                compression=uproot.compression.Compression.from_code_pair(
-                    compression_code, compression_level
-                ),
-            )
-
-        try:
-            file = uproot.open(input_file)
-        except FileNotFoundError:
-            if skip_bad_files:
-                continue
-            msg = "File: {input_file} does not exist or is corrupt."
-            raise FileNotFoundError(msg) from None
-        if same_names:
-            for key in keys:
-                try:
-                    file[key]
-                except ValueError:
-                    if not union:
-                        continue
-                    msg = "Union key filter error."
-                    raise ValueError(msg) from None
-                if len(file[key].axes) == 1:
-                    h_sum = hadd_1d(destination, file, key, first)
-
-                elif len(file[key].axes) == 2:
-                    h_sum = hadd_2d(destination, file, key, first)
-
-                else:
-                    h_sum = hadd_3d(destination, file, key, first)
-
-        else:
-            n_keys = file.keys(filter_classname="TH[1|2|3][I|S|F|D|C]", cycle=False)
-            for i, _value in enumerate(keys):
-                if len(file[n_keys[i]].axes) == 1:
-                    h_sum = hadd_1d(destination, file, keys[i], first, n_key=n_keys[i])
-
-                elif len(file[n_keys[i]].axes) == 2:
-                    h_sum = hadd_2d(destination, file, keys[i], first, n_key=n_keys[i])
-
-                else:
-                    h_sum = hadd_3d(destination, file, keys[i], first, n_key=n_keys[i])
-
-                if h_sum is not None:
-                    file_out[keys[i]] = h_sum
-
-        first = False
-        file.close()
-
-
-def main():
-    """
-    Implementation of cmd-line executables.
-    """
-    argparser = argparse.ArgumentParser(description="Hadd ROOT histograms with Uproot")
-    argparser.add_argument("destination", type=str, help="path of output file")
-    argparser.add_argument(
-        "input_files",
-        type=str,
-        nargs="+",
-        help="list or directory (glob syntax accepted) of input files",
-    )
-    argparser.add_argument(
-        "-f",
-        "--force",
-        action="store_true",
-        default=True,
-        help="force overwrite of output file",
-    )
-    argparser.add_argument(
-        "-a", "--append", action="store", default=False, help="append to existing file"
-    )
-    argparser.add_argument(
-        "-c",
-        "--compression",
-        action="store",
-        default="lz4",
-        help="set compression level between 1-9",
-    )
-    argparser.add_argument(
-        "-c[0-9]",
-        "--compression_level",
-        action="store",
-        default=1,
-        help="set compression level between 1-9",
-    )
-    argparser.add_argument(
-        "-k",
-        "--skip_bad_files",
-        action="store",
-        default=False,
-        help="corrupt or non-existent input files are ignored",
-    )
-    argparser.add_argument(
-        "-u",
-        action="union",
-        default=True,
-        help="all histograms get copied to new file, only those with same name get added",
-    )
-
-    args = argparser.parse_args()
-
-    hadd(
-        args.destination,
-        args.input_file,
-        force=args.force,
-        append=args.append,
-        compression=args.compression,
-        compression_level=args.compression_level,
-        skip_bad_files=args.skip_bad_files,
-        union=args.union,
-    )
