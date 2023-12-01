ca972e4a153e03959d81dc78e36f6c6948cf1d36
Zoë Bilodeau <zobilodeau@gmail.com>
2023-12-01 11:48:56 +0100

    trailing whitespace

--- src/odapt/operations/merge.py
+++ src/odapt/operations/merge.py
@@ -32,7 +32,7 @@ def hadd_and_merge(
         files (Str or list of str): List of local ROOT files to read histograms from.
             May contain glob patterns.
         branch_types (dict or pairs of str → NumPy dtype/Awkward type): Name and type specification for the TBranches.
-        fieldname_separator (str): Character that separates TBranch names for columns, used 
+        fieldname_separator (str): Character that separates TBranch names for columns, used
             for grouping columns (to avoid duplicate counters in ROOT file).
         field_name (callable of str → str): Function to generate TBranch names for columns
             of an Awkward record array or a Pandas DataFrame.
