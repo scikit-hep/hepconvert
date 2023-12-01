9836c56ec52d52cec32b9e2631e47cb2dba998b0
Zoë Bilodeau <zobilodeau@gmail.com>
2023-12-01 11:33:48 +0100

    Searching for way to deal with grouping with Parquet

--- src/odapt/operations/parquet_to_root.py
+++ src/odapt/operations/parquet_to_root.py
@@ -1,8 +1,11 @@
 import awkward as ak
 import dask_awkward as dak
 import uproot
-def parquet_to_root(in_file,
+def parquet_to_root(
+    in_file,
     out_file,
+    name,
+    fieldname_separator="_",
     *,
     columns=None,
     row_groups=None,
@@ -13,27 +16,32 @@ def parquet_to_root(in_file,
     generate_bitmasks=False,
     highlevel=True,
     behavior=None,
+    compression="lz4",
+    compression_level=1,
     ):
+
     # dak.from_parquet(read_path, split_row_groups=True)
     metadata = ak.metadata_from_parquet(in_file)
-    # print("Columns?", type(metadata['form'].content))
-    # print("Columns?", metadata['form']._column_types())
-    of = uproot.recreate(out_file)
-    first = True
-    # of.mktree(branch_types={metadata['form'].fields: })
-    for i in range(metadata['num_row_groups']): # So, we want Tbaskets to be about 1 mb? I forget. But something specific
-        if first:
-            print(type(ak.from_parquet(in_file, row_groups=[i])))
-            data = ak.from_parquet(in_file, row_groups=[i])
-            print(i.fields)
-            of.mktree()
-            print(type(data))
-            # of['tree'] = 
-            first = False
-        else:
-            of['tree'].extend(ak.from_parquet(in_file, row_groups=[i])) # Set size with extend.
     
-from skhep_testdata import data_path
+    of = uproot.recreate(out_file)
 
-# parquet_to_root("/Users/zobil/Documents/odapt/tests/samples/parquet_test.parquet", "/Users/zobil/Documents/odapt/tests/samples/parquet_test.parquet")
-parquet_to_root("https://pivarski-princeton.s3.amazonaws.com/chicago-taxi.parquet", "large_out.root")
+    data = ak.from_parquet(in_file, row_groups=[0])
+    print(type(data))
+    typenames = {name: data[name].type for name in data.fields}
+    of.mktree(name, typenames)
+    of[name].extend(data)
+
+    for i in range(1,metadata['num_row_groups']):
+        of[name].extend(ak.from_parquet(in_file, row_groups=[i])) # Set size with extend.
+    
+# from skhep_testdata import data_path
+
+# file = uproot.open(data_path("uproot-HZZ.root"))
+
+# array = file['events'].arrays()
+
+# ak.to_parquet(array, "uproot-HZZ.parquet")
+
+parquet_to_root("tests/samples/uproot-HZZ.parquet", "tests/samples/parquet_test.parquet", "tree")
+test = uproot.open("tests/samples/parquet_test.parquet")
+print(test[""].show())
