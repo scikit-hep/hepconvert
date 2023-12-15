75f2e355d630152e8606c2ec6fa182db9fcd2a35
Zoë Bilodeau <70441641+zbilodea@users.noreply.github.com>
2023-12-15 13:18:16 +0100

    Update __init__.py

--- src/odapt/root/__init__.py
+++ src/odapt/root/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
 from odapt.root.copy import copy_root  # noqa: F401
-from odapt.root.hadd import hadd_1d, hadd_2d, hadd_3d  # noqa: F401
+from odapt.root.hadd import hadd_1d, hadd_2d, hadd_3d, hadd  # noqa: F401
 from odapt.root.merge import hadd_and_merge  # noqa: F401
