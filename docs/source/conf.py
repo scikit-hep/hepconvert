06d19c4d996caab82a10ea6d31a87e27d20455ed
Zoë Bilodeau <zobilodeau@gmail.com>
2024-01-11 13:48:00 +0100

    trying to fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ release = "1.1.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc"]
+extensions = ['sphinx_rtd_theme', "sphinx.ext.autodoc"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -30,7 +30,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 html_static_path = []
 
 # Additional stuff
