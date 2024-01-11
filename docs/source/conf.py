14a2900631f24aa535fdb85c5beaa92ff2e31694
Zoë Bilodeau <70441641+zbilodea@users.noreply.github.com>
2024-01-11 13:52:45 +0100

    fix: webdocs (#39)
    * found error in conf.py path
    
    * added requirement for sphinx_rtd_theme (is this okay?) and extension for rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ release = "1.1.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -31,7 +31,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_static_path = []
 
 # Additional stuff
 master_doc = "index"
